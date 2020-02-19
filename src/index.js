ef55cd7a53682bd3c3e11c1182c31559f394e497
Andrei Kozik <13pgs1.kozik.a@pdu.by>
2020-02-20 01:06:19 +0300

    complite tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,97 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    numberObj = {
+        "0": "zero",
+        "1": "one",
+        "2": "two",
+        "3": "three",
+        "4": "four",
+        "5": "five",
+        "6": "six",
+        "7": "seven",
+        "8": "eight",
+        "9": "nine",
+        "10": "ten",
+        "11": "eleven",
+        "12": "twelve",
+        "13": "thirteen",
+        "14": "fourteen",
+        "15": "fifteen",
+        "16": "sixteen",
+        "17": "seventeen",
+        "18": "eighteen",
+        "19": "nineteen",
+        "20": "twenty",
+        "30": "thirty",
+        "40": "forty",
+        "50": "fifty",
+        "60": "sixty",
+        "70": "seventy",
+        "80": "eighty",
+        "90": "ninety",
+        "100": "one hundred",
+        "200": "two hundred",
+        "300": "three hundred",
+        "400": "four hundred",
+        "500": "five hundred",
+        "600": "six hundred",
+        "700": "seven hundred",
+        "800": "eight hundred",
+        "900": "nine hundred"
+    };
+
+    if (String(number).length === 1) {
+        for (key in numberObj) {
+            if (String(number) === key) return numberObj[key];
+        }
+    } else if (String(number).length === 2) {
+        let result = "";
+        let des = Math.floor(number / 10) * 10;
+        let ed = number - des;
+        if (des < 20) {
+            des = des + ed;
+            ed = 0;
+        }
+        for (key in numberObj) {
+            if (String(des) === key) {
+                result = result + numberObj[key];
+            }
+        }
+
+        for (key in numberObj) {
+            if (ed === 0) return result;
+            if (String(ed) === key) {
+                result = result + " " + numberObj[key];
+            }
+        }
+        return result;
+
+    } else if (String(number).length === 3) {
+        let result = "";
+        const sot = Math.floor(number / 100) * 100;
+        let des = Math.floor((number - sot) / 10) * 10;
+        let ed = number - (sot + des);
+        if(des<20) {
+            des = des + ed
+            ed = 0
+        }
+        for (key in numberObj) {
+            if (String(sot) === key) {
+                result = result + numberObj[key];
+            }
+        }
+        for (key in numberObj) {
+            if (des === 0) return result;
+            if (String(des) === key) {
+                result = result + " " + numberObj[key];
+            }
+        }
+
+        for (key in numberObj) {
+            if(ed === 0) return result
+            if (String(ed) === key) {
+                result = result + " " + numberObj[key];
+            }
+        }
+        return result;
+    }
+};
